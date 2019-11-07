cf980c7244c47c5ab179885baa4378cffa99cfe8
dufei <dufei@daling.com>
2019-11-07 18:28:57 +0800

    保护测试

--- src/chat_item.tsx
+++ src/chat_item.tsx
@@ -22,7 +22,7 @@ class ChatItem extends Component<Props, State> {
 	componentDidMount() {
 		console.log(NativeModules)
 		try {
-			this.listener = CMR.IsExistCLEFile('asdasdsa', (err, res) => {
+			this.listener = CMR.IsExistCLEFile('asdasdsa', (err:any, res:any) => {
 				console.log(res)
 			});
 		} catch (error) {
